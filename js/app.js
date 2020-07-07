82a4f679b4860a084cbf174e224ff1a7235c4613
Heansell Tahay <hdtahay@gmail.com>
2020-07-07 16:27:23 -0600

    cambios de rutas por prod

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 
+let url = window.location.href;
+
+let swLocation = '/twittor/sw.js';
 
 if(navigator.serviceWorker){
 
-    navigator.serviceWorker.register('/sw.js');
+    if(url.includes('localhost')){
+        swLocation = '/sw.js';
+    }
+
+    navigator.serviceWorker.register(swLocation);
+
+
 
 }
 
